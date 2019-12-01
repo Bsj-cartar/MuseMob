521390bac3fc435c24be785ab65e2c64d244f553
SJ <bsj-cartar@gmail.com>
2019-12-02 06:45:51 +0900

    19.12.02 last2

--- src/main/java/com/web/MuseMobApplication.java
+++ src/main/java/com/web/MuseMobApplication.java
@@ -2,15 +2,19 @@ package com.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@SpringBootApplication
-public class MuseMobApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MuseMobApplication.class, args);
+
+@SpringBootApplication 
+public class MuseMobApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) { 
+		SpringApplication.run(MuseMobApplication.class, args); } 
+	
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+		return builder.sources(MuseMobApplication.class); } 
 	}
 
-	
-}
